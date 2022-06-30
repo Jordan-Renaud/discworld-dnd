9e9651a6c9692e31796d18eba844be7331288010
Jordan Renaud <jordan.dj.renaud@gmail.com>
2022-06-30 19:32:53 +0100

    set up character page and remove junk

---
+++ pages/character.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Character() {
+  return (
+    <div>
+      <Head>
+        <title>Character</title>
+        <meta name="description" content="Character" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <button>Create New Character</button>
+    </div>
+  );
+}

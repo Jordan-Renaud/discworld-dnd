657224f58664d31b21d825bb8b9742f3d3f3b899
Jordan Renaud <jordan.dj.renaud@gmail.com>
2022-07-01 09:45:11 +0100

    set up styling for character page

---
+++ data/defaultCharacters.js
@@ -0,0 +1,43 @@
+export const defaultCharacters = [
+  {
+    name: "Death",
+    quote: (
+      <>
+        What is there in this world that truly makes living worthwhile? <br />
+        Death thought about it. &quot;CATS&quot; he said eventually. &quot;CATS
+        ARE NICE.&quot;
+      </>
+    ),
+    image: "/death.jpeg",
+    imageAlt: "Death playing the guitar.",
+    description:
+      "Death is not invisible. Most people just refuse to acknowledge him for who he is, unless he insists. Under normal circumstances, only those of a magical disposition, children and cats can see him, or allow themselves to see him.",
+  },
+  {
+    name: "The Librarian",
+    quote: "ook",
+    image: "/librarian.webp",
+    imageAlt: "A orang-utan reading a book.",
+    description: (
+      <>
+        The Librarian is known for his violent reaction whenever he hears anyone
+        refer to him as a &quot;monkey&quot; (orang-utans are apes). He speaks
+        an elaborate language whose vocabulary consists of the single word Ook
+        (and its antonym &quot;eek&quot; - where &quot;ook&quot; means yes,
+        &quot;eek&quot; tends to mean no). Nonetheless, most people seem to be
+        able to understand him.
+      </>
+    ),
+  },
+  // { name: "", quote: "", image: "", description: "" },
+];
+
+export const characterClasses = [
+  { name: "", quote: "", image: "", description: "" },
+  { name: "", quote: "", image: "", description: "" },
+  { name: "", quote: "", image: "", description: "" },
+  { name: "", quote: "", image: "", description: "" },
+  { name: "", quote: "", image: "", description: "" },
+  { name: "", quote: "", image: "", description: "" },
+  { name: "", quote: "", image: "", description: "" },
+];
--- pages/characters.js
+++ pages/characters.js
@@ -1,8 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
-import librarian from "../public/librarian.webp";
-import death from "../public/death.jpeg";
+import { defaultCharacters } from "../data/defaultCharacters";
 
 export default function Characters() {
   return (
@@ -15,45 +14,29 @@ export default function Characters() {
       <main className={styles.main}>
         <h1 className={styles.title}>Characters</h1>
         <div className={styles.grid}>
-          <div className={styles.simpleCard}>
-            <h2>Death</h2>
-            <q>
-              What is there in this world that truly makes living worthwhile?
-              <br />
-              Death thought about it. &quot;CATS&quot; he said eventually.
-              &quot;CATS ARE NICE.&quot;
-            </q>
-            <Image
-              src={death}
-              width="716"
-              height="1077"
-              alt="Death playing the guitar."
-            />
-            <p>
-              Death is not invisible. Most people just refuse to acknowledge him
-              for who he is, unless he insists. Under normal circumstances, only
-              those of a magical disposition, children and cats can see him, or
-              allow themselves to see him.
-            </p>
-          </div>
-          <div className={styles.simpleCard}>
-            <h2>The Librarian</h2>
-            <q>ook</q>
-            <Image
-              src={librarian}
-              width="308"
-              height="365"
-              alt="A orang-utan reading a book."
-            />
-            <p>
-              The Librarian is known for his violent reaction whenever he hears
-              anyone refer to him as a &quot;monkey&quot; (orang-utans are
-              apes). He speaks an elaborate language whose vocabulary consists
-              of the single word Ook (and its antonym &quot;eek&quot; - where
-              &quot;ook&quot; means yes, &quot;eek&quot; tends to mean no).
-              Nonetheless, most people seem to be able to understand him.
-            </p>
-          </div>
+          {defaultCharacters.map((character) => {
+            return (
+              <div className={styles.simpleCard} key={character.name}>
+                <h2>{character.name}</h2>
+                <div className={styles.grid}>
+                  <div className={styles.imageContainer}>
+                    <Image
+                      src={character.image}
+                      width="300"
+                      height="300"
+                      alt={character.imageAlt}
+                      layout="intrinsic"
+                      objectFit="cover"
+                    />
+                  </div>
+                  <div className={styles.quoteContainer}>
+                    <q>{character.quote}</q>
+                  </div>
+                </div>
+                <p className={styles.text}>{character.description}</p>
+              </div>
+            );
+          })}
         </div>
       </main>
     </div>

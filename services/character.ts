871b9e9f7288f3212e3b27d7ea3f3d7b6f5a6aa4
Seth Corker <seth.corker@gmail.com>
2022-07-02 13:35:43 +0100

    add prisma and use TS

---
+++ services/character.ts
@@ -0,0 +1,10 @@
+import prisma from "../lib/prisma";
+
+export async function getAllCharacters() {
+  return prisma.character.findMany()
+}
+
+// TODO: Accept character
+export async function createCharacter() {
+  return prisma.character.create({ data: { name: 'test' } })
+}

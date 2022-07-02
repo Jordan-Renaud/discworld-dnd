4638ecdf939e0ffe1ec9130275be58fccd8b5a27
Jordan Renaud <jordan.dj.renaud@gmail.com>
2022-07-02 17:23:32 +0100

    Add questions partial

--- components/BackstoryQuestionaire.js
+++ components/BackstoryQuestionaire.js
@@ -1,5 +1,26 @@
 import styles from "../styles/Home.module.css";
+import { backstoryQuestions } from "../data/backstoryQuestions";
 
 export default function BackstoryQuestionaire({ character }) {
-  return <div>BackstoryQuestionaire</div>;
+  return (
+    <div>
+      <h2>BackstoryQuestionaire</h2>
+      <form>
+        <ul>
+          {backstoryQuestions.map((question, index) => (
+            <li key={index}>
+              <label htmlFor={`Question ${index}`}>{question.question}</label>
+              <br />
+
+              <input
+                type="text"
+                id={`Question ${index}`}
+                name={`Question ${index}`}
+              />
+            </li>
+          ))}
+        </ul>
+      </form>
+    </div>
+  );
 }
---
+++ data/backstoryQuestions.js
@@ -0,0 +1,30 @@
+export const backstoryQuestions = [
+  {
+    question: "What's three beliefs that your character holds?",
+    choices: [
+      "Thou shalt honour the law.",
+      "Conspiracies rule the world. Wake up sheeple!",
+      "Kinda racist against trolls.",
+      "Hallucinates a giant hand in the sky that directs the character's destiny.",
+      "Gods are real but they're just old magic users that got really good.",
+    ],
+  },
+  {
+    question:
+      "Name two of your character’s most interesting personality traits that a stranger would remember after encountering them in a crowded tavern.",
+    choices: ["", ""],
+  },
+  {
+    question: "What’s one lie your character believes about themselves?",
+    choices: ["", ""],
+  },
+  {
+    question: "What is your character going to do when the adventure's over?",
+    choices: ["", ""],
+  },
+  {
+    question:
+      "How would your character react to seeing someone being mugged in an alleyway by a man wielding a sword?",
+    choices: ["", ""],
+  },
+];
